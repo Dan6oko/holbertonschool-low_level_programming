1150d29877726440b6a8914cca0e5b0d7ae13231
Dan6oko <dansokoalign@gmail.com>
2023-02-21 11:03:53 -0800

    assign a random number to the variable n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,17 +12,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 0)
-
 {
 printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
 printf("%d is zero\n", n);
-} 
+}
 else if (n < 0)
 {
 printf("%d is negatif\n", n);
-} 
- return (0);
+}
+return (0);
 }

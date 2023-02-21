9c2df253fd1166956f70c4b3861c503206d6609e
Dan6oko <dansokoalign@gmail.com>
2023-02-21 11:17:28 -0800

    assign a random number to the variable n

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/** more headers goes there
+* main - main fonction to generate a random number
+* betty style doc for function main goes there **/
 int main(void)
 {
 int n;
@@ -21,7 +21,7 @@ printf("%d is zero\n", n);
 }
 else if (n < 0)
 {
-printf("%d is negatif\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }

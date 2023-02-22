681ea82ac04b979873b08da66f0ae6a99d664a96
Dan6oko <dansokoalign@gmail.com>
2023-02-22 04:39:17 -0800

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,9 +21,8 @@ if (n % 10 == 0)
 {
 printf("%d and is 0\n" , n);
 }
- else if ((n % 10 < 6) && (n % 10 < 0))
+else if (n % 10 < 6)
 {
 printf("%d and is less than 6 and not 0\n" , n);
-}
 return (0);
 }

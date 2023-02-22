1dec1d908944f43df0175ed7c515444debc28e39
Dan6oko <dansokoalign@gmail.com>
2023-02-22 06:30:15 -0800

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point of the program
- *
- * Return: 0 if the program completed successfully, otherwise a non-zero value.
- */
+* main - Entry point of the program
+*
+* Return: 0 if the program completed successfully, otherwise a non-zero value.
+*/
 int main(void)
 {
 int n;
@@ -24,6 +24,6 @@ printf("Last digit of %d is %d and is 0\n", n, R);
 else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, R);
- }
+}
 return (0);
 }
